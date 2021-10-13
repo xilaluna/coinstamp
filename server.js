936a15647350e97ab2dccaa2c6f6851cd4bb8bb5
Xila Luna <xilaluna2@gmail.com>
2021-10-13 11:19:28 -0700

    [update] database connection

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 const express = require("express")
 const exphbs = require("express-handlebars")
-const mongoose = require("mongoose")
 
 const app = express()
 
@@ -15,23 +14,10 @@ app.set("view engine", "handlebars")
 
 const indexRouter = require("./routes/index")
 const cartRouter = require("./routes/cart")
+const connectDatabase = require("./data/db")
 
 app.use("/", indexRouter)
 app.use("/cart", cartRouter)
-
-const connectDatabase = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    })
-    console.log("MongoDB connected")
-  } catch (err) {
-    console.log("Failed to connect to MongoDB", err)
-  }
-}
 connectDatabase()
 
 const PORT = process.env.PORT || 3000
