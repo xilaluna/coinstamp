5abdb49e3e8b357ac370790a2c02f96644af25db
Xila Luna <xilaluna2@gmail.com>
2021-10-13 01:32:03 -0700

    [add] connect database

--- server.js
+++ server.js
@@ -19,6 +19,21 @@ const cartRouter = require("./routes/cart")
 app.use("/", indexRouter)
 app.use("/cart", cartRouter)
 
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    })
+    console.log("MongoDB connected")
+  } catch (err) {
+    console.log("Failed to connect to MongoDB", err)
+  }
+}
+connectDatabase()
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server listening on port : ${PORT}`)
