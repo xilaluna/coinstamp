3120889d619a34ca050a1cf920857638bbb22e88
Xila Luna <xilaluna2@gmail.com>
2021-10-12 18:02:09 -0700

    [add] home

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const exphbs = require("express-handlebars")
 
 const app = express()
 
+app.use(express.static("public"))
+
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
