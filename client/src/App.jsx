93fc35534973bb1bd24f4da7740812d802840600
Xila Luna <xilaluna2@gmail.com>
2021-11-09 21:10:46 -0800

    [upd] reconfig app.jsx and navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,14 @@
 import React from "react"
+import Navbar from "./components/Navbar/Navbar"
+import { CssBaseline } from "@mui/material"
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Navbar />
+      <main></main>
+    </>
   )
 }
 
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { AppBar, Toolbar, Typography, Button } from "@mui/material"
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
