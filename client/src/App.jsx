05e32e108b67ec1bf87b5908c7511edf8c9bebaa
Xila Luna <xilaluna2@gmail.com>
2021-11-10 15:46:32 -0800

    [add] theme provider in root and init stamp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react"
 import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
 import { CssBaseline } from "@mui/material"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+
+const theme = createTheme()
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Navbar />
       <main></main>
-    </>
+      <Footer />
+    </ThemeProvider>
   )
 }
 
---
+++ client/src/components/Stamp/Stamp.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Stamp = () => {
+  return <div></div>
+}
+
+export default Stamp
