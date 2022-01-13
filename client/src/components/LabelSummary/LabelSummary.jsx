9d964c3d6c6cfdc26112dff1b93771836e1cac7c
xiluna <xilaluna2@gmail.com>
2022-01-12 20:24:17 -0800

    update label sum

--- client/src/components/LabelSummary/LabelSummary.jsx
+++ client/src/components/LabelSummary/LabelSummary.jsx
@@ -5,33 +5,34 @@ import Address from "./Address"
 import { useSelector } from "react-redux"
 
 const LabelSummary = () => {
-  //const addresses = useSelector((state) => state.stamp.addresses)
+  const addresses = useSelector((state) => state.stamp.addresses)
+  console.log(addresses)
   return (
     <React.Fragment>
       <Grid container spacing={4}>
         <Grid item md={6}>
           <Address
             header={"From"}
-            name={"John Doe"}
-            street1={"417 montgomery street"}
+            name={addresses.to_address.name}
+            street1={addresses.to_address.street1}
             street2={"None"}
-            city={"San Francisco"}
-            state={"CA"}
-            zip={"94104"}
-            country={"US"}
+            city={addresses.to_address.city}
+            state={addresses.to_address.state}
+            zip={addresses.to_address.zip}
+            country={addresses.to_address.street1.country}
             phone={"None"}
           />
         </Grid>
         <Grid item md={6}>
           <Address
             header={"To"}
-            name={"George Costanza"}
-            street1={"1 E 161st St."}
+            name={addresses.from_address.name}
+            street1={addresses.from_address.street1}
             street2={"None"}
-            city={"New York"}
-            state={"NY"}
-            zip={"10451"}
-            country={"US"}
+            city={addresses.from_address.city}
+            state={addresses.from_address.state}
+            zip={addresses.from_address.zip}
+            country={addresses.from_address.street1.country}
             phone={"None"}
           />
         </Grid>
