6b0548382f1fdc12da05fad74a102d3e1e5d3c02
xiluna <xilaluna2@gmail.com>
2022-01-13 22:52:20 -0800

    [update]

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // init cors
-app.use(cors({ origin: "*" }))
+app.use(cors())
 
 // routes init
 app.use("/stamp", stampRouter)
