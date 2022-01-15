9de2c68ec3508d9e8ac4c6b622f3120497a55b8b
xiluna <xilaluna2@gmail.com>
2022-01-15 01:07:48 -0800

    [init] redis

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ import "./config/config.js"
 import express from "express"
 import cors from "cors"
 import stampRouter from "./routes/stampRoutes.js"
+import session from "express-session"
 import path from "path"
+import { createClient } from "redis"
+import * as connectRedis from "connect-redis"
 
 // init app
 const app = express()
+
+// init redis
+const redisClient = createClient()
+redisClient.unref()
+redisClient.on("error", (error) => console.log("Redis Client Error", error))
+
+const RedisStore = connectRedis(session)
+let store = new RedisStore({ client: redisClient })
 
 // JSON parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // init cors
-app.use(cors())
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
 
 // routes init
 app.use("/stamp", stampRouter)
