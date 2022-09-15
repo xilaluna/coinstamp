f7e2b44a819f40ceadd2a3819d0fd36cd7f86bb7
Xila Luna <xilaluna2@gmail.com>
2022-09-15 01:32:16 -0700

    style: update with new absoulute path and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,8 @@ import { withTRPC } from "@trpc/next";
 import { SessionProvider } from "next-auth/react";
 import type { AppType } from "next/dist/shared/lib/utils";
 import superjson from "superjson";
-import type { AppRouter } from "../server/router";
-import "../styles/globals.css";
+import type { AppRouter } from "@/server/router";
+import "@/styles/globals.css";
 
 const MyApp: AppType = ({
   Component,
