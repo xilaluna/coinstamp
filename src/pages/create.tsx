b527f2705764f79cad75c48cbb3047ef62d47d41
Xila Luna <xilaluna2@gmail.com>
2022-09-16 01:38:42 -0700

    feat: init create forms and add styling

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -8,8 +8,220 @@ const Create: NextPage = () => {
       <Head>
         <title>Create - CoinStamp</title>
       </Head>
-      <main className="container-page">
+      <main className="container-secondary">
         <Heading title="Create" subtitle="Create a new stamp" />
+
+        <section className="grid grid-cols-2 gap-10">
+          <div className="border-color col-span-2 rounded-md border p-5 ">
+            <h2 className="heading-color pb-5 text-xl">Carrier Information</h2>
+            <div className="grid grid-cols-2 gap-5">
+              <label className="block">
+                <span>Carrier</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>Package Type</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+            </div>
+          </div>
+
+          <div className="border-color col-span-2 rounded-md border p-5">
+            <h2 className="heading-color pb-5 text-xl">
+              Dimensions &amp; Weight
+            </h2>
+            <div className="grid grid-cols-12 gap-5">
+              <label className="col-span-4 block">
+                <span>Length</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="col-span-4 block">
+                <span>Width</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="col-span-4 block">
+                <span>Height</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+
+              <label className="col-span-6 block">
+                <span>Pounds</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="col-span-6 block">
+                <span>Ounces</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+            </div>
+          </div>
+
+          <div className="border-color rounded-md border p-5">
+            <h2 className="heading-color pb-5 text-xl">From</h2>
+            <div className="grid gap-5">
+              <label className="block">
+                <span>Country</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>Name</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>Address</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+
+              <label className="block">
+                <span>Apt/Suite</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>City</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>State</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>Zip</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>Phone</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+            </div>
+          </div>
+
+          <div className="border-color rounded-md border p-5">
+            <h2 className="heading-color pb-5 text-xl">To</h2>
+            <div className="grid gap-5">
+              <label className="block">
+                <span>Country</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>Name</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>Address</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+
+              <label className="block">
+                <span>Apt/Suite</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>City</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>State</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>Zip</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>Phone</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+            </div>
+          </div>
+
+          <div className="border-color col-span-2 rounded-md border p-5">
+            <h2 className="heading-color pb-5 text-xl">Submit</h2>
+            <div className="grid gap-5">
+              <label className="block">
+                <span>Country</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+              <label className="block">
+                <span>Name</span>
+                <input
+                  type="text"
+                  className="border-color mt-1 block w-full rounded-md"
+                />
+              </label>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
