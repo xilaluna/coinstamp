3d98502330a774c5e4b8cc22e6be9a5fea16102e
Xila Luna <xilaluna2@gmail.com>
2022-09-15 18:07:07 -0700

    feat: init the beginning of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <header className="page-container">Navbar</header>;
+  return (
+    <header className="page-container py-5">
+      <Link href={"/"}>
+        <a>CoinStamp</a>
+      </Link>
+    </header>
+  );
 };
 
 export default Navbar;
