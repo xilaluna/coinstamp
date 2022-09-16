f74567978f72f3f1a2f1080c826a2ab57b8a2d9f
Xila Luna <xilaluna2@gmail.com>
2022-09-15 20:02:31 -0700

    feat: update navbar comp with nav and links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,35 @@
-import React from "react";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <header className="page-container py-5">
+    <header className="page-container border-color flex flex-row border-b py-5">
       <Link href={"/"}>
         <a>CoinStamp</a>
       </Link>
+      <nav className="ml-auto">
+        <ul className="flex space-x-5">
+          <li>
+            <Link href={"/new"}>
+              <a>New Stamp</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/rates"}>
+              <a>Rates</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/cart"}>
+              <a>Cart</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/order"}>
+              <a>Order</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
