fed17e9ed148e18b8c5a91ae18b678b00d775aae
Xila Luna <xilaluna2@gmail.com>
2022-10-07 03:36:31 -0700

    feat: added btn as a comp

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+  return <button {...props}>{children}</button>;
+};
+
+export default Button;
