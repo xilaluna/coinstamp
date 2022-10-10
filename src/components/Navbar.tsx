42ff2c37df8bff4f3c07413d9a12324633312d2f
Xila Luna <xilaluna2@gmail.com>
2022-10-09 20:29:52 -0700

    feat: styled navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 const Navbar: React.FC = () => {
   return (
-    <header>
-      <h1>Navbar</h1>
+    <header className="border-color sticky top-0 z-10 border-b backdrop-blur-md">
+      <div className="page-width flex flex-row py-5">
+        <Link href={"/"}>
+          <a className="pl-2 hover:text-indigo-500">CoinStamp</a>
+        </Link>
+        <nav className="ml-auto flex space-x-5">
+          <Link href={"/create"}>
+            <a className="hover:text-indigo-500">Create</a>
+          </Link>
+          <Link href={"/rates"}>
+            <a className="hover:text-indigo-500">Rates</a>
+          </Link>
+          <Link href={"/cart"}>
+            <a className="hover:text-indigo-500">Cart</a>
+          </Link>
+          <Link href={"/order"}>
+            <a className="hover:text-indigo-500">Order</a>
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
