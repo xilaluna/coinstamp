eda40fb1feef96f99291bcc5629a24753c1cb41c
Xila Luna <xilaluna2@gmail.com>
2022-10-10 21:27:11 -0700

    feat: added forms to create and added address info to rates

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -4,7 +4,217 @@ import type { NextPage } from "next";
 const Create: NextPage = () => {
   return (
     <Layout heading="Create" description="Create a new stamp">
-      <div>Create</div>
+      <section className="grid grid-cols-2 gap-10">
+        <div className="border-color col-span-2 rounded-md border p-5 ">
+          <h2 className="heading-color pb-5 text-xl">Carrier Information</h2>
+          <div className="grid grid-cols-2 gap-5">
+            <label className="block">
+              <span>Carrier</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>Package Type</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+          </div>
+        </div>
+
+        <div className="border-color col-span-2 rounded-md border p-5">
+          <h2 className="heading-color pb-5 text-xl">
+            Dimensions &amp; Weight
+          </h2>
+          <div className="grid grid-cols-12 gap-5">
+            <label className="col-span-4 block">
+              <span>Length</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="col-span-4 block">
+              <span>Width</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="col-span-4 block">
+              <span>Height</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+
+            <label className="col-span-6 block">
+              <span>Pounds</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="col-span-6 block">
+              <span>Ounces</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+          </div>
+        </div>
+
+        <div className="border-color rounded-md border p-5">
+          <h2 className="heading-color pb-5 text-xl">From</h2>
+          <div className="grid gap-5">
+            <label className="block">
+              <span>Country</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>Name</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>Address</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+
+            <label className="block">
+              <span>Apt/Suite</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>City</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>State</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>Zip</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>Phone</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+          </div>
+        </div>
+
+        <div className="border-color rounded-md border p-5">
+          <h2 className="heading-color pb-5 text-xl">To</h2>
+          <div className="grid gap-5">
+            <label className="block">
+              <span>Country</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>Name</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>Address</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+
+            <label className="block">
+              <span>Apt/Suite</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>City</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>State</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>Zip</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>Phone</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+          </div>
+        </div>
+
+        <div className="border-color col-span-2 rounded-md border p-5">
+          <h2 className="heading-color pb-5 text-xl">Submit</h2>
+          <div className="grid gap-5">
+            <label className="block">
+              <span>Country</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+            <label className="block">
+              <span>Name</span>
+              <input
+                type="text"
+                className="border-color mt-1 block w-full rounded-md"
+              />
+            </label>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 };
--- src/pages/rates.tsx
+++ src/pages/rates.tsx
@@ -6,6 +6,28 @@ import testdata from "@/data/testdata";
 const Rates: NextPage = () => {
   return (
     <Layout heading="Rates" description="Rates for your postage">
+      <section className="flex space-x-10">
+        <div className="border-color flex w-full flex-col rounded-md border p-5">
+          <h2 className="heading-color pb-5 text-lg">From</h2>
+          <p>{testdata.from_address.name}</p>
+          <p>{testdata.from_address.street1}</p>
+          <p>
+            {testdata.from_address.city}, {testdata.from_address.state}{" "}
+            {testdata.from_address.zip}
+          </p>
+          <p>{testdata.from_address.country}</p>
+        </div>
+        <div className="border-color flex w-full flex-col rounded-md border p-5">
+          <h2 className="heading-color pb-5 text-lg">To</h2>
+          <p>{testdata.to_address.name}</p>
+          <p>{testdata.to_address.street1}</p>
+          <p>
+            {testdata.to_address.city}, {testdata.to_address.state}{" "}
+            {testdata.to_address.zip}
+          </p>
+          <p>{testdata.to_address.country}</p>
+        </div>
+      </section>
       <section className="border-color flex flex-col items-center justify-center rounded-md border p-5">
         <table className="w-full table-auto">
           <thead>
